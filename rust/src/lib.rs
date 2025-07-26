a857e365e628f9aedf6f947eea85c5a9168cfdae
Nitesh Kumar Debnath <30214255+veechiecooqu@users.noreply.github.com>
2025-07-26 15:48:07 +0530

    I forget the code for fibonacci everytime so I will just commit it

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,11 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn fib(n: u64) -> u128 {
+    (0..n).fold((0, 1), |(a, b), _| (a + b, a)).0
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
