c8c43f90e19ff74fa23af8f6e5d7dc39d2e851c2
Nitesh Kumar Debnath <30214255+nitdn@users.noreply.github.com>
2025-09-19 19:45:18 +0530

    Merge with git

--- rust-workspaces/subcrate-example/src/lib.rs
+++ rust-workspaces/subcrate-example/src/lib.rs
@@ -2,6 +2,18 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn test_random_spaces() -> u64 {
+    // test for bad spaces, will not compile
+    if true {
+        unimplemented!()
+    }
+    unimplemented!()
+}
+
+pub fn test_pattern_matching((1 | 2 | 3 | _): u64) {
+    unimplemented!("why did i do this")
+}
+
 // pub fn test_random_spaces() -> u64 {
 //     // test for bad spaces, will not compile
 //     if true {
